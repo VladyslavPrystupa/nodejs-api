1d9745e269321b2fe29bb4e7bce460915f6e8c95
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-05-28 01:21:37 +0300

    edit json

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
+
+mongoose.connect().then().catch();
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log('Server running. Use our API on port: 3000');
+});
