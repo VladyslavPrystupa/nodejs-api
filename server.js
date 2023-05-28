a5f29c1b5ef8c73c226b99a9877a26d4fbf4b689
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-05-28 20:54:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 
 mongoose.set('strictQuery', false);
 
-const { DB_HOST, PORT } = process.env;
-
+// const { DB_HOST, PORT } = process.env;
+// const DB_HOST =
+//   'mongodb+srv://Vladyslav:qwerty1@cluster0.lzabk4l.mongodb.net/contacts_db?retryWrites=true&w=majority';
+const PORT = 3000;
 mongoose
-  .connect(DB_HOST)
+  .connect(
+    'mongodb+srv://Vladyslav:qwerty1@cluster0.lzabk4l.mongodb.net/contacts_db?retryWrites=true&w=majority'
+  )
   .then(() => {
     app.listen(PORT);
     console.log('Database connection successful');
